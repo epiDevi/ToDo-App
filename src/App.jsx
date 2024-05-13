47ca47d395e8350e8254c1a8fa036c64e17a47eb
Sepideh Ghanbary <3pideh.ghanbary@gmail.com>
2024-05-13 13:48:03 +0200

    create Projeckt

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [toDos, setToDos] = useState([])
+
+  return (
+    <>
+      
+      <h1>ToDo-App</h1>
+      
+    </>
+  )
+}
+
+export default App
